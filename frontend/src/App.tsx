214bf722403ad508550ded92fe1f5bb9e7efb5ec
Nisal1990 <35098989+Nisal1990@users.noreply.github.com>
2023-07-15 14:08:40 +0530

    add login and signup UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route index element={<Welcome/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </div>
   );
